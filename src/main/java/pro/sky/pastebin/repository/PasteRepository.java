a764391e034dd025d76df2581847d77fc6cddbcf
jalig <117361652+jalig@users.noreply.github.com>
2023-04-17 01:10:10 +0400

    ну теперь точно все

--- src/main/java/pro/sky/pastebin/repository/PasteRepository.java
+++ src/main/java/pro/sky/pastebin/repository/PasteRepository.java
@@ -3,8 +3,6 @@ package pro.sky.pastebin.repository;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pro.sky.pastebin.model.Paste;
 import pro.sky.pastebin.model.enums.PasteStatus;
@@ -22,6 +20,5 @@ public interface PasteRepository extends JpaRepository<Paste, String>, JpaSpecif
     List<Paste> findAll(Specification<Paste> specification);
 
     void deleteAllByExpiredTimeIsBefore(Instant now);
-
 
 }
